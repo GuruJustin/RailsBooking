723a451f9edd698302681ae90fc2600d839fedad
gurujustin <meanjustin2020@hotmail.com>
2020-12-21 14:18:54 -0500

    new Doc show

--- Blog/app/controllers/welcome_controller.rb
+++ Blog/app/controllers/welcome_controller.rb
@@ -1,30 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  def
-
-  def destory
-  end
-
-  
-  private
-    def find_doc
-    end
-
-    def doc_params
-    end
 end
