876f326155b7b44547bf5f6a8a242ecb79303ca2
gurujustin <meanjustin2020@hotmail.com>
2020-12-21 16:34:54 -0500

    Add stylesheet and scss

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcome#index"
 
+  authenticated :user do
+    root "doc#index", as: "authenticated_root"
+  end
+
   resources :docs
 end
