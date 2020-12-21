5cc04f586028cd62890529f5ce50cfdd9021ae4a
gurujustin <meanjustin2020@hotmail.com>
2020-12-21 13:17:31 -0500

    model

--- Blog/app/controllers/welcome_controller.rb
+++ Blog/app/controllers/welcome_controller.rb
@@ -1,4 +1,30 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  def
+
+  def destory
+  end
+
+  
+  private
+    def find_doc
+    end
+
+    def doc_params
+    end
 end
